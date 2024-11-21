3a472f4a2827c40a1c6d233193ba73c53136769f
armandopinedam <111319135+armandopinedam@users.noreply.github.com>
2024-11-21 11:28:26 -0600

    Update SpeedMonitor.cpp

--- SpeedMonitor.cpp
+++ SpeedMonitor.cpp
@@ -2,36 +2,31 @@
 #include <random> 
 #include <string>
 
+
+#include "SpeedMonitor.h"
+
 #include "IOTCloudCommunicator.h"
 #include "BNFSpeedSensor.h"
 
 using namespace std;
 
-class SpeedMonitor{
-    private : int _speedThreshold;
-  
-      public:
-          SpeedMonitor(int speedThreshold): _speedThreshold{speedThreshold} {}
-          void monitor(){
-            if(_speedThreshold  < 1 || _speedThreshold > 100){
-              cout<<"_speedThreshold value must be in the ramge {1-100} "<<_speedThreshold<<endl;
-            }
-              
-            BNFSpeedSensor speedSensorInstance;
-            IOTCloudCommunicator cloudCommuniccator;
-            int currentSpeedInKmph=speedSensorInstance.getCurrentSpeed();
-            
-            cout<<"Current Speed In Kmph "<<currentSpeedInKmph<<endl;
-            if(currentSpeedInKmph > _speedThreshold){
-                  double mph = currentSpeedInKmph * 0.621371;
-                  string message="Current Speed in Miles "+ to_string(mph);
-                 int statusCode= cloudCommuniccator.pushMessage(message);
-                  if(statusCode > 400){
-                      //Log Message to Console
-                       cout<<"Error In Communication Unable to Contact Server "<<message<< endl;
-                  }
-                 
-            }
-              
+  void SpeedMonitor::monitor(){
+    if(_speedThreshold  < 1 || _speedThreshold > 100){
+      cout<<"_speedThreshold value must be in the ramge {1-100} "<<_speedThreshold<<endl;
+    }
+      
+    BNFSpeedSensor speedSensorInstance;
+    IOTCloudCommunicator cloudCommuniccator;
+    int currentSpeedInKmph=speedSensorInstance.getCurrentSpeed();
+    
+    cout<<"Current Speed In Kmph "<<currentSpeedInKmph<<endl;
+    if(currentSpeedInKmph > _speedThreshold){
+          double mph = currentSpeedInKmph * 0.621371;
+          string message="Current Speed in Miles "+ to_string(mph);
+         int statusCode= cloudCommuniccator.pushMessage(message);
+          if(statusCode > 400){
+              //Log Message to Console
+               cout<<"Error In Communication Unable to Contact Server "<<message<< endl;
           }
-};
+         
+    }
