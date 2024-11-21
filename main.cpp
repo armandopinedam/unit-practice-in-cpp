129cdb29ed0f35d3d889c931251f0a41209f1c70
armandopinedam <111319135+armandopinedam@users.noreply.github.com>
2024-11-21 11:42:00 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "SpeedMonitor.h"
+#include "ConsoleLogger.h"
 
 int main(){
-  SpeedMonitor instance {10};
+  ConsoleLogger consoleLogger;
+  SpeedMonitor instance {10, consoleLogger};
   instance.monitor();
   instance.monitor();
   instance.monitor();
